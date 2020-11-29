21ae884226c62dd963a18dd16729bff213d52c49
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-11-29 22:17:46 +0900

    ファイルを分割する

---
+++ greengrocer_main.rb
@@ -0,0 +1,21 @@
+require "./methods.rb"
+
+# 商品データ
+products = [
+  {name: "トマト", price: 100},
+  {name: "きゅうり", price: 200},
+  {name: "玉ねぎ", price: 300},
+  {name: "なす", price: 400}
+]
+
+# 定数を設定。ここでの変更がコード全体に反映される
+FIRST_PRODUCT_NUM = 1 # 表示する最初の商品の番号（変更可）
+LAST_PRODUCT_NUM = (FIRST_PRODUCT_NUM + products.size) - 1 # 表示する最後の商品の番号（変更不可）
+DISCOUNT_STANDARD_VALUE = 5 # 割引を適用する個数の基準値（変更可）
+DISCOUNT＿RATE = 0.1 # 割引率（変更可）
+AFRER_DISCOUNT_RATE = 1 - DISCOUNT＿RATE # 割引後の本体価格の割合（変更不可）
+
+disp_products(products) # 商品を表示
+chosen_product = choose_product(products) # 商品を選択
+quantity_of_product = decide_quantity(chosen_product) # 商品の個数を決定
+calculate_charges(chosen_product, quantity_of_product) # 合計金額を計算
