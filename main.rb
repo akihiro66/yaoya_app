86ff369685cc585b8eb77f352497e8acf9994369
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-11-29 12:56:36 +0900

    商品の表示

--- main.rb
+++ main.rb
@@ -1,2 +1,13 @@
-puts "hello"
-puts 3 + 4
+# 商品データ
+products = [
+  {name: "トマト", price: 100},
+  {name: "きゅうり", price: 200},
+  {name: "玉ねぎ", price: 300},
+  {name: "なす", price: 400}
+]
+
+# 商品を表示
+puts "いらっしゃいませ！商品を選んで下さい。"
+products.each.with_index(1) do |product,i|
+  puts "#{i}.#{product[:name]}(#{product[:price]}円)"
+end
