80a276282999f65c1ccc3406cafb07f7c2e3cd54
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-11-30 12:41:06 +0900

    ファイルを分割

--- greengrocer_main.rb
+++ greengrocer_main.rb
@@ -10,26 +10,28 @@ product_params1 = [
   {name: "なす", price: 400}
 ]
 
-product_params2 = [
-  {name: "パセリ", price: 100},
-  {name: "ブロッコリー", price: 150}
+# product_params1の商品を持つ八百屋の開店
+greengrocer1 = Greengrocer.new(product_params1)
+
+# 追加商品データ
+adding_products1 = [
+  {name: "ごぼう", price: 250},
+  {name: "れんこん", price: 350}
 ]
 
-# 八百屋の開店
-greengrocer1 = Greengrocer.new(product_params1)
-greengrocer2 = Greengrocer.new(product_params2)
+# 商品を登録（adding_products1 の商品を追加）
+greengrocer1.register_product(adding_products1)
+
+# お客さんの来店
+user = User.new
+
 # 商品を表示
 greengrocer1.disp_products
-greengrocer2.disp_products
-
-# 定数を設定。ここでの変更がコード全体に反映される
-FIRST_PRODUCT_NUM = 1 # 表示する最初の商品の番号（変更可）
-LAST_PRODUCT_NUM = (FIRST_PRODUCT_NUM + products.size) - 1 # 表示する最後の商品の番号（変更不可）
-DISCOUNT_STANDARD_VALUE = 5 # 割引を適用する個数の基準値（変更可）
-DISCOUNT＿RATE = 0.1 # 割引率（変更可）
-AFRER_DISCOUNT_RATE = 1 - DISCOUNT＿RATE # 割引後の本体価格の割合（変更不可）
-
-disp_products(products) # 商品を表示
-chosen_product = choose_product(products) # 商品を選択
-quantity_of_product = decide_quantity(chosen_product) # 商品の個数を決定
-calculate_charges(chosen_product, quantity_of_product) # 合計金額を計算
+# 商品を選択
+user.choose_product(greengrocer1.products)
+# 個数を質問
+greengrocer1.ask_quantity(user.chosen_product)
+# 個数を決定
+user.decide_quantity
+# 金額金額を計算
+greengrocer1.calculate_charges(user)
